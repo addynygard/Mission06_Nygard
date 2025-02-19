af53718b461e08af067825fd0eb539565fa94eae
Addy <dragyn.addy@gmail.com>
2025-02-19 13:39:59 -0700

    changed my form and .cs file to match the database called CollectionofMovie.sqlite

--- Mission06_Nygard/Models/Movie.cs
+++ Mission06_Nygard/Models/Movie.cs
@@ -8,32 +8,34 @@ namespace Mission06_Nygard.Models
         // Creates a MovieID property that is an integer and is required
         [Key]
         public int MovieID { get; set; }
+        
         // Creates a Category property that is a string and is required
-
-        [Required]
-        public string Category { get; set; } = string.Empty;
+        public int? CategoryId { get; set; } // do I need this int.empty??
+        
         // Creates a Title property that is a string and is required
-
         [Required]
         public string Title { get; set; } = string.Empty;
+        
         // Creates a Year property that is an integer and is required
-
         [Required]
         public int Year { get; set; }
+        
         // Creates a Rating property that is a string and is required
+        public string? Rating { get; set; } = string.Empty; // this is now nullable
 
-        [Required]
-        public string Rating { get; set; } = string.Empty;
         // Creates a Director property that is a string and is required
+        public string? Director { get; set; } = string.Empty; // this is now nullable
 
-        [Required]
-        public string Director { get; set; } = string.Empty;
         // Creates a Edited property that is a boolean and is required
-
+        [Required]
         public bool Edited { get; set; }
 
         // Creates a LentTo property that is a string and is nullable
         public string? LentTo { get; set; } // Nullable
+
+        // Creates a CopiedToPlex property that is a string and is required
+        [Required]
+        public string CopiedToPlex { get; set; } = string.Empty;
 
         // Creates a Notes property that is a string and is nullable
         [StringLength(25, ErrorMessage = "Notes must be 25 characters or less.")]
